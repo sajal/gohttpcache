39f59392e630228e01dda89ddd339a938e0fedea
Sajal Kayan <sajal@turbobytes.com>
2014-08-03 06:52:51 +0700

    Vary compliant proxy

--- proxy.go
+++ proxy.go
@@ -1,22 +1,221 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
+	"fmt"
+	"github.com/valyala/ybc/bindings/go/ybc"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 var originmapping map[string]string
 
+var (
+	objcache  ybc.Cacher
+	metacache ybc.Cacher
+)
+
+var client http.RoundTripper
+
+type MetaItem struct {
+	Header  http.Header
+	ObjKey  []byte
+	Fetched time.Time
+	Status  int
+}
+
+type MetaObj struct {
+	Vary []string
+}
+
 func buildcachekey(r *http.Request, origin string) (key []byte) {
 	//for now ignore vary and things to keep simple
-	key = append([]byte(origin), []byte(r.RequestURI)...)
+	key = append([]byte(r.Method), []byte(origin)...)
+	key = append(key, []byte(r.RequestURI)...)
 	return
+}
+
+func getvary(key []byte) (vary []string, err error) {
+	m, err := metacache.Get(key)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	rdr := bytes.NewReader(m)
+	dec := gob.NewDecoder(rdr)
+	//start := time.Now()
+	meta := &MetaObj{}
+	err = dec.Decode(meta)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	vary = meta.Vary
+	return
+}
+
+func fetchupstream(r *http.Request, origin string) (item *ybc.Item, err error) {
+	log.Println(r)
+	url := fmt.Sprintf("http://%s%s", origin, r.RequestURI)
+	log.Println(url)
+	req, err := http.NewRequest(r.Method, url, nil)
+	if err != nil {
+		return
+	}
+	for k, v := range r.Header {
+		if k != http.CanonicalHeaderKey("Host") {
+			for _, val := range v {
+				req.Header.Add(k, val)
+			}
+		}
+	}
+	resp, err := client.RoundTrip(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	// load into cache...
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	var buffer bytes.Buffer
+	hdrobj := &MetaItem{Header: resp.Header, Status: resp.StatusCode, Fetched: time.Now()}
+	enc := gob.NewEncoder(&buffer)
+	err = enc.Encode(hdrobj)
+	if err != nil {
+		return
+	}
+	hdrbyt := buffer.Bytes()
+	itemsize := len(body) + len(hdrbyt) + 2
+	//TODO: rebuild key here, and update vary in meta
+	key := buildcachekey(r, origin)
+	metaobj := &MetaObj{}
+	for _, k := range strings.Split(resp.Header.Get("Vary"), ",") {
+		metaobj.Vary = append(metaobj.Vary, http.CanonicalHeaderKey(strings.Trim(k, " ")))
+	}
+
+	var metabuffer bytes.Buffer
+	metaenc := gob.NewEncoder(&metabuffer)
+	err = metaenc.Encode(metaobj)
+	if err != nil {
+		return
+	}
+	metacache.Set(key, metabuffer.Bytes(), ybc.MaxTtl)
+	key = appendvary(key, r.Header, metaobj.Vary)
+	txn, err := objcache.NewSetTxn(key, itemsize, ybc.MaxTtl)
+	if err != nil {
+		return
+	}
+	err = storeheaders(hdrbyt, txn)
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+	_, err = txn.Write(body)
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+	item, err = txn.CommitItem()
+	return
+}
+
+func storeheaders(hdrbyt []byte, w io.Writer) (err error) {
+	if len(hdrbyt) > 65025 {
+		//TODO err
+	}
+	var hdrsize int16 = int16(len(hdrbyt))
+	err = binary.Write(w, binary.LittleEndian, hdrsize)
+	if err != nil {
+		return
+	}
+	_, err = w.Write(hdrbyt)
+	return
+}
+
+func loadmeta(r io.Reader) (hdr MetaItem, err error) {
+	var sizebuf [2]byte
+	_, err = r.Read(sizebuf[:])
+	if err != nil {
+		return
+	}
+	buf := bytes.NewReader(sizebuf[:])
+	var hdrsize int16
+	err = binary.Read(buf, binary.LittleEndian, &hdrsize)
+	if err != nil {
+		return
+	}
+	hdrbytes := make([]byte, hdrsize)
+	_, err = r.Read(hdrbytes)
+	if err != nil {
+		return
+	}
+	rdr := bytes.NewReader(hdrbytes)
+	dec := gob.NewDecoder(rdr)
+	//start := time.Now()
+	err = dec.Decode(&hdr)
+	return
+}
+
+func getitem(key []byte, r *http.Request, origin string) (item *ybc.Item, err error) {
+	item, err = objcache.GetDeItem(key, time.Second)
+	if err != nil {
+		item, err = fetchupstream(r, origin)
+	}
+	return
+}
+
+func serveitem(w http.ResponseWriter, item *ybc.Item) {
+	defer item.Close()
+	meta, err := loadmeta(item)
+	if err != nil {
+		log.Println(err)
+		servefail(w)
+		return
+	}
+	for k, v := range meta.Header {
+		if k != http.CanonicalHeaderKey("Date") {
+			for _, val := range v {
+				w.Header().Add(k, val)
+			}
+		}
+	}
+	w.Header().Set("Age", fmt.Sprintf("%9.f", time.Since(meta.Fetched).Seconds()))
+	w.WriteHeader(meta.Status)
+	io.Copy(w, item)
+}
+
+func servefail(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadGateway)
+}
+
+func appendvary(key []byte, headers http.Header, vary []string) []byte {
+	for _, k := range vary {
+		key = append([]byte(headers.Get(k)), key...)
+	}
+	return key
 }
 
 func cachecheckhandler(w http.ResponseWriter, r *http.Request, origin string) {
 	key := buildcachekey(r, origin)
 	log.Println(string(key))
+	vary, err := getvary(key)
+	key = appendvary(key, r.Header, vary)
+	log.Println(string(key))
+	item, err := getitem(key, r, origin)
+	if err != nil {
+		servefail(w)
+	} else {
+		serveitem(w, item)
+	}
 }
 
 func hosthandler(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +232,16 @@ func main() {
 	originmapping = make(map[string]string, 2)
 	originmapping["cdn.cdnplanet.com"] = "www.cdnplanet.com"
 	originmapping["cdn.turbobytes.com"] = "www.turbobytes.com"
+	client = &http.Transport{}
+	metacacheconfig := ybc.Config{
+		MaxItemsCount: ybc.SizeT(500 * 1000), //500k keys.. i.e. ip+cdn combos
+	}
+	metacache, _ = metacacheconfig.OpenCache(true)
+
+	objcacheconfig := ybc.Config{
+		MaxItemsCount: ybc.SizeT(500 * 1000), //500k keys.. i.e. ip+cdn combos
+	}
+	objcache, _ = objcacheconfig.OpenCache(true)
 
 	log.Println("Hello Proxy")
 	srv := &http.Server{
